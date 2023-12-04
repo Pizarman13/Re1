9320da1589b754b729e18e8ca3112e962c7791c9
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-12-04 21:26:18 +0000

    ej3.1

--- circulo.h
+++ circulo.h
@@ -1,5 +1,16 @@
+#include <cmath>
+
 namespace Geometria {
+    class Circulo {
+        private:
+        double radio;
+        public:
+        Circulo(double r) : radio(r) {}
     double calcularAreaCirculo(double radio) {
-        return 3.14 * radio * radio;
+        return M_PI * radio * radio;
     }
+    double calcularPerimetroCirculo(double radio) {
+        return 2 * M_PI * radio;
+    }
+    };
 }
--- mainGeoOp.cpp
+++ mainGeoOp.cpp
@@ -10,7 +10,10 @@ int main() {
 
     cout << "Hello World!" << endl;
 
-    cout << "Area del circulo: " << Geometria::calcularAreaCirculo(5) << endl;
+    Geometria::Circulo c(5);
+    
+    cout << "Area del circulo: " << c.calcularAreaCirculo(5) << endl;
+    cout << "Perimetro del circulo: " << c.calcularPerimetroCirculo(5) << endl;
     cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << endl;
 
     return 0;
